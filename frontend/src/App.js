f0bf7f43a8dbe0b43fd3026c5e64a146ab6d8f53
damianduralek <damian.duralek@gmail.com>
2022-06-20 19:06:11 +0200

    test3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,12 @@ import NoticePage from "./containers/NoticePage";
 import AddNotice from "./containers/AddNotice";
 import "bootstrap/dist/css/bootstrap.min.css";
 //damian sie sprzedal
+//dupa chuj
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Navbar />
         <Navbar />
         <Routes>
           <Route path="/" exact element={<NoticesList />} />
