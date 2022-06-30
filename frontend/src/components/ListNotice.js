48258f010da111347396bd7ddb6cf8ec307161f4
damianduralek <damian.duralek@gmail.com>
2022-06-30 23:44:25 +0200

    lista ogłoszeń + searchbar

--- frontend/src/components/ListNotice.js
+++ frontend/src/components/ListNotice.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Notice.css" 
+import "./ListNotice.css" 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import styles from "./Notice.css"
+
 const ListNotice = ({ notice }) => {
   console.log(notice)
   return (
@@ -21,83 +24,209 @@ const ListNotice = ({ notice }) => {
     //   </div>
     // </div>
 
-    <div class="col-md-3">
 
 
 
-    				<div class="card">
+<div class="col-md-6 notice">
+			<Link to={`/notice/${notice.id}`}>
+                <div class="block product no-border z-depth-2-top z-depth-2--hover">
+                    <div class="block-image">
+                        <a href="#">
+                            <img src={notice.NoticeImg} class="img-center"/>
+                        </a>
+                        <span class="product-ribbon product-ribbon-right product-ribbon--style-1 bg-blue text-uppercase">New</span>
+                    </div>
+                    <div class="block-body text-center">
+                        <h1 class="heading heading-5 strong-600 text-capitalize">
+                            
+							{notice.NoticeTitle}
+                            
+                        </h1>
+                        <p class="product-description">
+						{notice.NoticeDescription}
+                        </p>
+                        <p class="product-description">
+						<hr/><strong>{notice.NoticeCredit} zł</strong>
+                        </p>
+                        <div class="product-buttons mt-4">
+                            <div class="row align-items-center">
+                                <div class="col-2">
+                                    <button type="button" class="btn-icon" data-toggle="tooltip" data-placement="top" title="" data-original-title="Favorite">
+                                        <FontAwesomeIcon icon="fa-thin fa-heart" />
+                                    </button>
+                                </div>
+                                <div class="col-2">
+                                    <button type="button" class="btn-icon" data-toggle="tooltip" data-placement="top" title="" data-original-title="Compare">
+                                        <i class="fa fa-share"></i>
+                                    </button>
+                                </div>
+                                <div class="col-8">
+                                    <button type="button" class="btn btn-block btn-primary btn-circle btn-icon-left">
+                                        <i class="fa fa-shopping-cart"></i>Add to cart
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+				</Link>
+            </div>
+			
 
-    					<div class="image-container">
 
-    						<div class="first">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* {<div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8 col-lg-6 col-xl-4">
+        <div class="card text-black">
+          <i class="fab fa-apple fa-lg pt-3 pb-1 px-3"></i>
+          <img
+            src={notice.NoticeImg}
+            class="card-img-top"
+            alt="Apple Computer"
+          />
+          <div class="card-body">
+            <div class="text-center">
+              <h5 class="card-title">{notice.NoticeTitle}</h5>
+              <p class="text-muted mb-4">Apple pro display XDR</p>
+            </div>
+            <div>
+              <div class="d-flex justify-content-between">
+                <span>Pro Display XDR</span><span>$5,999</span>
+              </div>
+              <div class="d-flex justify-content-between">
+                <span>Pro stand</span><span>$999</span>
+              </div>
+              <div class="d-flex justify-content-between">
+                <span>Vesa Mount Adapter</span><span>$199</span>
+              </div>
+            </div>
+            <div class="d-flex justify-content-between total font-weight-bold mt-4">
+              <span>Total</span><span>$7,197.00</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <div class="col-md-3">
+
+
+
+    // 				<div class="card">
+
+    // 					<div class="image-container">
+
+    // 						<div class="first">
     							
-    							<div class="d-flex justify-content-between align-items-center">
+    // 							<div class="d-flex justify-content-between align-items-center">
 
-    							<span class="discount">-25%</span>
-    							<span class="wishlist"><i class="fa fa-heart-o"></i></span>
+    // 							<span class="discount">-25%</span>
+    // 							<span class="wishlist"><i class="fa fa-heart-o"></i></span>
     							
 
-    						    </div>
-    						</div>
+    // 						    </div>
+    // 						</div>
 
-    						<img src={notice.NoticeImg} class="img-fluid rounded thumbnail-image" />
+    // 						<img src={notice.NoticeImg} class="img-fluid rounded thumbnail-image" />
     						
 
-    					</div>
+    // 					</div>
 
 
-    					<div class="product-detail-container p-2">
+    // 					<div class="product-detail-container p-2">
 
-    							<div class="d-flex justify-content-between align-items-center">
+    // 							<div class="d-flex justify-content-between align-items-center">
 
-    								<h5 class="dress-name">White traditional long dress</h5>
+    // 								<h5 class="dress-name">White traditional long dress</h5>
 
-    								<div class="d-flex flex-column mb-2">
+    // 								<div class="d-flex flex-column mb-2">
 
-    									<span class="new-price">$3.99</span>
-    									<small class="old-price text-right">$5.99</small>
-    								</div>
+    // 									<span class="new-price">$3.99</span>
+    // 									<small class="old-price text-right">$5.99</small>
+    // 								</div>
 
-    							</div>
+    // 							</div>
 
 
-    							<div class="d-flex justify-content-between align-items-center pt-1">
+    // 							<div class="d-flex justify-content-between align-items-center pt-1">
 
-    								<div class="color-select d-flex ">
+    // 								<div class="color-select d-flex ">
 
-    									<input type="button" name="grey" class="btn creme" />
-    									<input type="button" name="red" class="btn red ml-2" />
-    									<input type="button" name="blue" class="btn blue ml-2" />
+    // 									<input type="button" name="grey" class="btn creme" />
+    // 									<input type="button" name="red" class="btn red ml-2" />
+    // 									<input type="button" name="blue" class="btn blue ml-2" />
 
-    								</div>
+    // 								</div>
 
-    								<div class="d-flex ">
+    // 								<div class="d-flex ">
     									
-    									<span class="item-size mr-2 btn" type="button">S</span>
-    									<span class="item-size mr-2 btn" type="button">M</span>
-    									<span class="item-size btn" type="button">L</span>
+    // 									<span class="item-size mr-2 btn" type="button">S</span>
+    // 									<span class="item-size mr-2 btn" type="button">M</span>
+    // 									<span class="item-size btn" type="button">L</span>
 
-    								</div>
+    // 								</div>
     								
  
-    							</div>
+    // 							</div>
 
 
-    							<div class="d-flex justify-content-between align-items-center pt-1">
-    								<div>
-    									<i class="fa fa-star-o rating-star"></i>
-    									<span class="rating-number">4.8</span>
-    								</div>
+    // 							<div class="d-flex justify-content-between align-items-center pt-1">
+    // 								<div>
+    // 									<i class="fa fa-star-o rating-star"></i>
+    // 									<span class="rating-number">4.8</span>
+    // 								</div>
 
-    								<span class="buy">BUY +</span>
+    // 								<span class="buy">BUY +</span>
     								
-    							</div>
+    // 							</div>
 
     						
 
-    					</div>
+    // 					</div>
     					
-    				</div>
+    // 				</div>
 
 
 
@@ -106,11 +235,7 @@ const ListNotice = ({ notice }) => {
     				
 
 
-
-    				
-
-    			
-    		</div>
+	// 	</div>
 
 
   );
--- frontend/src/containers/NoticesList.js
+++ frontend/src/containers/NoticesList.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import ListNotice from '../components/ListNotice';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { toast } from "react-toastify";
 import axios from "axios";
-import {setAxiosAuthToken,  toastOnError } from "../utils/Utils";
+import { setAxiosAuthToken, toastOnError } from "../utils/Utils";
 import SearchBar from '../components/SearchBar';
 const NoticesList = () => {
 
@@ -11,24 +11,24 @@ const NoticesList = () => {
     const [filteredData, setFilteredData] = useState([]);
     const [searchingWord, setSearchingWord] = useState([])
 
-    useEffect(() =>{
+    useEffect(() => {
         getNotices()
-    },[]
+    }, []
     )
 
     let getNotices = async () => {
 
 
         axios
-        .get("http://127.0.0.1:8000/api/notices/")
-        .then(response => {
-            const data  = response.data;
-            setNotices(data)
-        })
-        .catch(error => {
+            .get("http://127.0.0.1:8000/api/notices/")
+            .then(response => {
+                const data = response.data;
+                setNotices(data)
+            })
+            .catch(error => {
 
-          toastOnError(error);
-        });
+                toastOnError(error);
+            });
 
 
         // let response = await fetch('http://127.0.0.1:8000/api/notices/')
@@ -36,45 +36,51 @@ const NoticesList = () => {
         // console.log(data)
         // setNotices(data)
     }
-    const handleFilter = (event) =>{
-       const searchWord = event.target.value
-       const newFilter = notices.filter((value) =>{
-        console.log(value.NoticeTitle)
-        return value.NoticeTitle.includes(searchWord)
-       })
-       setFilteredData(newFilter)
-       setSearchingWord(searchWord)
+    const handleFilter = (event) => {
+        const searchWord = event.target.value
+        const newFilter = notices.filter((value) => {
+            console.log(value.NoticeTitle)
+            return value.NoticeTitle.includes(searchWord)
+        })
+        setFilteredData(newFilter)
+        setSearchingWord(searchWord)
 
-       //console.log(searchWord.length)
+        //console.log(searchWord.length)
     }
-  return (
-    
-    <div className="search">
-            <div className="searchInputs">
-                <input type="text" placeholder="Szukaj ogłoszenie..." onChange={handleFilter}/>
-                <div className="searchIcon"></div>
+    return (
+
+        <div className="container">
+            <div className="d-flex justify-content-center h-100">
+                <div className="search">
+                    <input className="search-input" type="text" placeholder="Szukaj ogłoszenie..." onChange={handleFilter} />
+                </div>
             </div>
 
             <div className="dataResult"></div>
-        
-        {searchingWord.length !=0 ? (
-        <div className="notices">  
-        {
-        filteredData.map((notice, index) => (
-            <ListNotice key={index} notice={notice} />
-        ))}
+
+            {searchingWord.length != 0 ? (
+                <div class="container">
+                    <div class="shop-default shop-cards shop-tech">
+                        <div class="row">
+                            {
+                                filteredData.map((notice, index) => (
+                                    <ListNotice key={index} notice={notice} />
+                                ))}
+                        </div></div></div>
+            ) : (
+                <div class="container ">
+                    <div class="shop-default shop-cards shop-tech">
+                        <div class="row">
+                            {
+                                notices.map((notice, index) => (
+                                    <ListNotice key={index} notice={notice} />
+                                ))}
+                        </div></div></div>
+            )
+            }
         </div>
-        ) : (
-        <div className="notices">  
-        {
-        notices.map((notice, index) => (
-            <ListNotice key={index} notice={notice} />
-        ))}
-        </div>
-        )
-        }
-    </div>
-  )
+
+    )
 }
 // 2.09.48
 
